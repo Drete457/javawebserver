8ecadb0098e3df1297335a8314f356810e62d1eb
Filipe Mota <drete457@gmail.com>
2020-02-25 14:25:30 +0000

    now the server can send video to

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -6,12 +6,15 @@ import java.util.Scanner;
 
 public class Server {
 
-    private static int portNumber = 0;
-    private static Scanner listen = new Scanner(System.in);
-    private static ServerSocket serverSocket;
     private static Socket clientSocket;
+    private static String notFound = "resources/www/notFound.html";
 
     public static void main(String[] args) {
+
+        ServerSocket serverSocket;
+
+        Scanner listen = new Scanner(System.in);
+        int portNumber = 0;
 
         System.out.print("What port I will listen? ");
         portNumber = listen.nextInt();
@@ -30,60 +33,83 @@ public class Server {
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            String sentence = new String(in.readLine());
-            String[] request = sentence.split(" ");
+            String sentence = in.readLine();
+            if (sentence != null) {
+                String[] request = sentence.split(" ");
 
+                System.out.println(sentence);
 
-            System.out.println(sentence);
-
-            if (request[0].equals("GET")) {
-
-                if (request[1].equals("/index.html")) {
-                    String send = "HTTP/1.0 200 Document Follows\\r\\n" + "Content-Type: text/html; charset=UTF-8\r\n" + "Content-Length: 203 \r\n" + "\r\n";
-                    String path = "/Users/codecadet/Desktop/Exer/IDE/Webserver/src/Server/index.html";
-                    send(path, send);
-                    in.close();
-                } else if (request[1].equals("/gato.jpg")){
-                    String send = "HTTP/1.0 200 Document Follows\\r\\n" + "Content-Type: image/gato.jpg\n" + "Content-Length: 53873 \r\n" + "\r\n";
-                    String path = "/Users/codecadet/Desktop/Exer/IDE/Webserver/src/Server/gato.jpg";
-                    send(path, send);
-                    in.close();
-                }
-                else {
-                    String send = "HTTP/1.0 404 Not Found\r\n" + "Content-Type: text/html; charset=UTF-8\r\n" + "Content-Length: 160 \r\n" + "\r\n";
-                    String path = "/Users/codecadet/Desktop/Exer/IDE/Webserver/src/Server/notFound.html";
-                    send(path, send);
-                    in.close();
+                if (request[0].equals("GET")) {
+                    if (request[1].equals("/")) {
+                        sendHeader(notFound);
+                        in.close();
+                    } else {
+                        String path = "resources/www" + request[1];
+                        sendHeader(path);
+                        in.close();
+                    }
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            e.getMessage();
         }
     }
-    private static void send(String path, String send) {
+
+    private static String getHeader(String path){
+        String headerReturn;
+        if (path.contains("notFound")){
+            headerReturn = "HTTP/1.0 404 Not Found\r\n"; }
+        else {
+            headerReturn = "HTTP/1.0 200 Document Follows\r\n"; }
+    return headerReturn;
+    }
+
+    private static String getFileType(String path) throws IOException {
+        String type;
+        if(path.contains("videogato.mp4")) {
+            type = "video/mp4";
+        } else {
+            type = new URL("file://" + path).openConnection().getContentType();
+        }
+        return type;
+    }
+
+    private static void sendHeader(String path) {
         try {
             File file = new File(path);
             FileInputStream readFile = new FileInputStream(file);
-            BufferedInputStream lineFile = new BufferedInputStream(readFile);
 
-            int finish = 0;
-            byte[] buffer = new byte[1024];
+
+            String header = getHeader(getFileType(path)) +
+            "Content-Type: " + getFileType(path) + "\r\n" +
+            "Content-Lenght " + file.length() + " \r\n" +
+            "\r\n";
+
+            System.out.println(header);
 
             OutputStream out = clientSocket.getOutputStream();
 
-
-            out.write(send.getBytes());
+            out.write(header.getBytes());
             out.flush();
-            System.out.println("sending file");
-            while ((finish = lineFile.read(buffer)) > 0){
-                out.write(buffer, 0, buffer.length);
-                out.flush();
 
-            }
+            sendFile( readFile, out);
+
             out.close();
             clientSocket.close();
         } catch (IOException e) {
             e.getMessage();
+        }
+    }
+
+    private static void sendFile(FileInputStream readFile, OutputStream out) throws IOException {
+        int finish = 0;
+        byte[] buffer = new byte[1024];
+        BufferedInputStream lineFile = new BufferedInputStream(readFile);
+
+        while ((finish = lineFile.read(buffer)) > 0){
+            out.write(buffer, 0, buffer.length);
+            out.flush();
+
         }
     }
 }
